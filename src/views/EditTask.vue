afc58478443a27c14d189fffd82c91fbb6133ffc
iffi47 <66529774+iffi47@users.noreply.github.com>
2023-05-04 10:22:26 +0500

    remove unnessary code

--- src/views/EditTask.vue
+++ src/views/EditTask.vue
@@ -21,7 +21,7 @@
 </template>
 <script setup>
 import axios from "axios";
-import { ref, reactive, onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import { API_URL } from '../api'
 import { useRoute, useRouter } from "vue-router"
 const route = useRoute()
